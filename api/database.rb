b4b9fbed59b47cd861b7e3e98738820a397b57a7
enrmarc <enrmarc@gmail.com>
2015-08-02 01:04:03 +0200

    Set dev and prod environment for mongolab

--- api/database.rb
+++ api/database.rb
@@ -9,7 +9,7 @@ class Database
 
   def initialize
     @config = YAML::load_file File.join(__dir__, 'config/config.yaml')
-    @uri = @config[ENV['env'] || 'dev']['db']['uri']
+    @uri = @config[@config['mode']]['db']['uri']
     @db = Mongo::Client.new(@uri)[:games]
   end
 end
